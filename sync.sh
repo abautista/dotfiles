703622cf4500a16d7e754bc15cba5ee2d214c19d
Alberto Bautista <alberto.bautista@me.com>
2013-03-27 00:56:44 +0100

    Added pathogen for vim plugins

--- sync.sh
+++ sync.sh
@@ -2,8 +2,15 @@
 
 DOTFILESDIR="$(pwd -P)"
 
-for DOTFILE in {vimrc,zshrc,gitconfig,gitignore_global}; do
+for DOTFILE in {vimrc,vim,zshrc,gitconfig,gitignore_global}; do
 	HOMEFILE="$HOME/.$DOTFILE"
 	DIRFILE="$DOTFILESDIR/$DOTFILE"
-	ln -sfv "$DIRFILE" "$HOMEFILE"
+    
+    if [ -L $HOMEFILE ] && ! [ -d $DOTFILE ] 
+    then
+	    ln -sfv "$DIRFILE" "$HOMEFILE"
+    else
+        rm -rv "$HOMEFILE"
+	    ln -sv "$DIRFILE" "$HOMEFILE"
+    fi
 done
