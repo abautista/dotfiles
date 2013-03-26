af2c962a5176eacc18d1a8ba461e29905d8e328d
Alberto Bautista <alberto.bautista@me.com>
2013-03-27 00:09:11 +0100

    Using symbolic links to dotfiles

--- sync.sh
+++ sync.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-git pull
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" --exclude "install_deps.sh" -av . ~
-}
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
-unset doIt
-source ~/.zshrc
+
+DOTFILESDIR="$(pwd -P)"
+
+for DOTFILE in {vimrc,zshrc,gitconfig,gitignore_global}; do
+	HOMEFILE="$HOME/.$DOTFILE"
+	DIRFILE="$DOTFILESDIR/$DOTFILE"
+	ln -sfv "$DIRFILE" "$HOMEFILE"
+done
---
+++ vimrc
@@ -0,0 +1,11 @@
+set nocompatible
+
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+
