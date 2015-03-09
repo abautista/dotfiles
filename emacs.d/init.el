545dba5b5c0c1c0c87f788dcb529615f1cd057d8
Alberto Bautista <alberto.bautista@me.com>
2015-03-09 08:30:22 +0100

    Updated emacs configuration.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,13 +1,27 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
+;; Clean the interface
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(menu-bar-mode -1)
 
+;; Projectile
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
+;; Autocomplete
 (require 'auto-complete-config)
 (ac-config-default)
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
 
+;; Keybindings
+(global-set-key (kbd "C-o") 'er/expand-region)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
-  
-
-  
+(setq helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match t)
