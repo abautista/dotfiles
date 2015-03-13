edbb691acdea3c4f9c2bd1936f45dd740f1d1337
Alberto Bautista <alberto.bautista@me.com>
2015-03-13 08:27:20 +0100

    Improving my emacs and tmux configuration.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,31 +1,40 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-;; Clean the interface
+;; clean the interface
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 
-;; Projectile
+;; disable auto-save and auto-backup
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+;; autocomplete
+(ac-config-default)
+
+;; snippets
+(yas-global-mode)
+
+;; projectile
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 
-;; Autocomplete
-(require 'auto-complete-config)
-(ac-config-default)
-(add-hook 'python-mode-hook 'jedi:setup)
+;; enable fuzzy matching
+(setq helm-semantic-fuzzy-match t)
+(setq helm-imenu-fuzzy-match t)
 
-;; Jedi
+;; jedi autocompletion
+(add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 (setq jedi:environment-root "default3")
 (setq jedi:environment-virtualenv (list "virtualenv" "-p" "/usr/local/bin/python3"))
 
-;; Keybindings
+;; keybindings
+(global-set-key (kbd "C-i") 'helm-semantic-or-imenu)
 (global-set-key (kbd "C-o") 'er/expand-region)
+(global-set-key (kbd "C-p") 'helm-projectile)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-(setq helm-semantic-fuzzy-match t
-      helm-imenu-fuzzy-match t)
