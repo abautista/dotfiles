ca54aaa3226df42992694a31f286f7cb4b804c5b
Alberto Bautista <alberto.bautista@me.com>
2015-04-06 08:29:59 +0200

    More emacs configuration.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,19 +3,30 @@
 (cask-initialize)
 
 ;; clean the interface
+(load-theme 'tango-dark)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(menu-bar-mode -1)
 
 ;; disable auto-save and auto-backup
 (setq auto-save-default nil)
 (setq make-backup-files nil)
+(setq scroll-conservatively most-positive-fixnum)
 
 ;; autocomplete
 (ac-config-default)
 
 ;; snippets
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"))
 (yas-global-mode)
+
+(defun create-snippet (filename)
+  (interactive "s")
+  (let ((mode (symbol-name major-mode)))
+    (find-file (format "~/.emacs.d/snippets/%s/%s" mode filename))
+    (snippet-mode)))
+
+(global-set-key (kbd "M-'") 'create-snippet)
 
 ;; projectile
 (projectile-global-mode)
@@ -26,12 +37,6 @@
 (setq helm-semantic-fuzzy-match t)
 (setq helm-imenu-fuzzy-match t)
 
-;; jedi autocompletion
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
-(setq jedi:environment-root "default3")
-(setq jedi:environment-virtualenv (list "virtualenv" "-p" "/usr/local/bin/python3"))
-
 ;; keybindings
 (global-set-key (kbd "C-o") 'er/expand-region)
 (global-set-key (kbd "C-p") 'helm-projectile)
@@ -40,14 +45,24 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-o") 'other-window)
 
-;; css mode config
+;; ruby
+(global-rinari-mode)
+
+;; python
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+(setq jedi:environment-root "default3")
+(setq jedi:environment-virtualenv (list "virtualenv" "-p" "/usr/local/bin/python3"))
+
+;; css
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . css-mode))
 (add-hook 'css-mode-hook
 	  (function (lambda ()
 		      (setq css-indent-offset 2))))
 
-;; web mode
+;; web
 (add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
 (setq web-mode-enable-current-column-highlight t)
 (setq web-mode-enable-auto-pairing t)
 
@@ -59,8 +74,8 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 
-(add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'web-mode-hook
 	  (function (lambda ()
 		      (setq web-mode-markup-indent-offset 2)
 		      (setq web-mode-css-indent-offset 2))))
+(put 'dired-find-alternate-file 'disabled nil)
