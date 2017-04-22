f565564cc53408d6091c1fecb17c2496eda674ce
Alberto Bautista <alberto.bautista@me.com>
2017-04-22 21:49:26 +0200

    Update emacs config.

---
+++ emacs.d.abautista/customizations/ui.el
@@ -0,0 +1,25 @@
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode 1)
+
+(setq inhibit-startup-message t)
+(setq frame-resize-pixelwise t)
+(setq scroll-conservatively most-positive-fixnum)
+
+(set-face-attribute 'default nil :font "Source Code Pro-13")
+
+;; no auto save files
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+;; global keybindings
+(global-set-key (kbd "M-o") 'other-window)
+
+(global-set-key (kbd "<f8>") 'first-error)
+(global-set-key (kbd "<f7>") 'previous-error)
+(global-set-key (kbd "<f9>") 'next-error)
+
+(global-set-key (kbd "<f11>") 'flop-frame)
--- emacs.d.abautista/init.el
+++ emacs.d.abautista/init.el
@@ -1,5 +1,73 @@
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(ido-mode t)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 
-(global-set-key (kbd "M-F") 'find-file-other-window)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;;;;
+;; Packages
+;;;;
+
+;; project interaction library for Emacs
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode))
+
+;; text completion framework for Emacs
+(use-package company
+  :ensure t
+  :init
+  (setq company-minimum-prefix-length 2)
+  (setq company-idle-delay 0.1)
+  :config
+  (global-company-mode))
+
+;; syntax checking for gnu emacs
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; a package in a league of it own
+(use-package helm
+  :ensure t
+  :init
+  (helm-mode 1)
+  :bind
+  ("C-x b" . helm-mini)
+  ("M-j" . helm-imenu)
+  ("C-x C-f" . helm-find-files)
+  ("M-x" . helm-M-x)
+  ("M-y" . helm-show-kill-ring)
+)
+
+;; mode for quickly browsing and editing plain notes
+(use-package deft
+  :ensure t
+  :bind ("C-x C-g" . deft)
+  :config
+  (setq deft-default-extension "org"))
+
+;; color-theme
+(use-package atom-one-dark-theme
+  :ensure t)
+
+;; Add a directory to our load path, so when you `load` things below,
+;; emacs knows where to look for the corresponding file.
+(add-to-list 'load-path "~/.emacs.d/customizations")
+
+;;;;
+;; Customizations
+;;;;
+
+;; User interface elements
+(load "ui.el")
+
+;; Org mode customizations
+(load "org-customizations.el")
